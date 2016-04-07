88584f9e57c5dc4b02e3cc22c5747146ac831b07
Manfern <ejilur@gmail.com>
2016-04-07 19:25:52 +0300

    Created basic structure of landing page

---
+++ send.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Спасибо за заявку </title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/main.css">
+
+
+	<link rel="stylesheet" href="css/demo.css" type="text/css" media="screen" />
+	<link rel="stylesheet" type="text/css" href="css/dialog.css" />
+	<link rel="stylesheet" href="css/forms.css" type="text/css" media="screen" />
+
+	<script src="js/modernizr.custom.js"></script>
+
+
+
+</head>
+
+<body>
+<header>
+
+﻿<div align='center' style='font-family: __Roboto_3;font-size:70px;color:#111111;line-height: normal;'><h1>Спасибо за заявку!</h1></div><div align='center' style='font-family: __Roboto_3;font-size:70px;color:#111111;line-height: normal;'><h1>В ближайшее время с Вами свяжется консультант</h1></div>	</header>
+<script type='text/javascript'>
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter = new Ya.Metrika({id:,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName('script')[0],
+        s = d.createElement('script'),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = (d.location.protocol == 'https:' ? 'https:' : 'http:') + '//mc.yandex.ru/metrika/watch.js';
+
+    if (w.opera == '[object Opera]') {
+        d.addEventListener('DOMContentLoaded', f, false);
+    } else { f(); }
+})(document, window, 'yandex_metrika_callbacks');
+</script>
+<noscript><div><img src='//mc.yandex.ru/watch/' style='position:absolute; left:-9999px;' alt='' /></div></noscript>
+</body>
+</html>
